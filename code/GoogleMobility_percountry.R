1e2a479b805cb289a4a7266b069fc1d6871e7de0
befriendabacterium <42059798+befriendabacterium@users.noreply.github.com>
2020-06-06 11:47:41 +0100

    made code more efficient (does same thing, fewer objects + lines)

--- code/GoogleMobility_percountry.R
+++ code/GoogleMobility_percountry.R
@@ -34,53 +34,35 @@ UK<-subset(Global_Mobility_Report,country_region_code == "GB")
 #UK<-UK[!(UK$sub_country%in%""), ]
 
 # Assigning country to UK districts ---------------------------------------
-#saveRDS(Districts_by_country,"Districts_by_country.RDS")
+
 Districts_by_country<-readRDS("inputdata/Districts_by_country.RDS")
 
 # Code to assign districts to the new data set ----------------------------
 #England
-England_col<-UK[(UK$sub_region_1%in%Districts_by_country[[1]]),]
-#plyr::count(UK$sub_region_1%in%Districts_by_country[[1]])
-England_col$sub_country<-England_col$sub_country%>%replace_na("England")
+UK$sub_country[(UK$sub_region_1%in%Districts_by_country[[1]])]<-"England"
 #Wales
-Wales_col<-UK[(UK$sub_region_1%in%Districts_by_country[[2]]),]
-Wales_col$sub_country<-Wales_col$sub_country%>%replace_na("Wales")
+UK$sub_country[(UK$sub_region_1%in%Districts_by_country[[2]])]<-"Wales"
 #Scotland
-Scotland_col<-UK[(UK$sub_region_1%in%Districts_by_country[[3]]),]
-Scotland_col$sub_country<-Scotland_col$sub_country%>%replace_na("Scotland")
+UK$sub_country[(UK$sub_region_1%in%Districts_by_country[[3]])]<-"Scotland"
 #Northern Ireland
-N_Ireland_col<-UK[(UK$sub_region_1%in%Districts_by_country[[4]]),]
-N_Ireland_col$sub_country<-N_Ireland_col$sub_country%>%replace_na("N Ireland")
-#Connect them all together
-UK<-rbind(England_col,Wales_col,Scotland_col,N_Ireland_col)
-
-# Subset the data to only present park data -------------------------------
-Park_UK <-subset(UK,select=c(sub_region_1,sub_country,date,parks_percent_change_from_baseline))
-#Park_UK<-na.omit(Park_UK)
+UK$sub_country[(UK$sub_region_1%in%Districts_by_country[[4]])]<-"N_Ireland"
 
 # Calculating UK mean and SE for each country division of the UK ----------
-Park_UK_mean<-tapply(Park_UK$parks_percent_change_from_baseline, list(Park_UK$sub_country,Park_UK$date),mean, na.rm=T)
-Park_UK_SE<-tapply(Park_UK$parks_percent_change_from_baseline, list(Park_UK$sub_country,Park_UK$date),std.error, na.rm=T)
+Park_UK_mean<-as.data.frame(t(tapply(UK$parks_percent_change_from_baseline, list(UK$sub_country,UK$date),mean, na.rm=T)))
+Park_UK_mean<-rownames_to_column(Park_UK_mean,var="Date")
+Park_UK_mean$Date<-as.Date(Park_UK_mean$Date)
 
-# Converting it into a dataframe -------------------------------------------
-#Mean Values
-UK_mean_df<-as.data.frame(t(Park_UK_mean))
-UK_mean_df<- rownames_to_column(UK_mean_df,var="Date")
-names(UK_mean_df)<-c("Date","England","N_Ireland","Scotland","Wales")
-UK_mean_df$Date<-as.Date(UK_mean_df$Date)
-#SE values
-UK_SE_df<-as.data.frame(t(Park_UK_SE))
-UK_SE_df<-rownames_to_column(UK_SE_df,var="Date")
-names(UK_SE_df)<-c("Date","England","N_Ireland","Scotland","Wales")
-UK_SE_df$Date<-as.Date(UK_SE_df$Date)
+Park_UK_SE<-as.data.frame(t(tapply(UK$parks_percent_change_from_baseline, list(UK$sub_country,UK$date),std.error, na.rm=T)))
+Park_UK_SE<-rownames_to_column(Park_UK_SE,var="Date")
+Park_UK_SE$Date<-as.Date(Park_UK_SE$Date)
 
 # Plots -------------------------------------------------------------------
 #England
 ENG_graph<-
-  ggplot(data=UK_mean_df,aes(x=Date,y=England))+
+  ggplot(data=Park_UK_mean,aes(x=Date,y=England))+
   geom_line()+
-  geom_ribbon(aes(ymin=England-UK_SE_df$England,
-                  ymax=England+UK_SE_df$England),alpha=0.4)+
+  geom_ribbon(aes(ymin=England-Park_UK_SE$England,
+                  ymax=England+Park_UK_SE$England),alpha=0.4)+
   coord_cartesian(ylim=c(-60,160))+
   geom_hline(yintercept=0)+ 
   theme(panel.background = element_rect(fill = "white", colour = "black",
@@ -94,10 +76,10 @@ ENG_graph<-
 
 #Wales
 WAL_graph<-
-  ggplot(data=UK_mean_df,aes(x=Date,y=Wales))+
+  ggplot(data=Park_UK_mean,aes(x=Date,y=Wales))+
   geom_line()+
-  geom_ribbon(aes(ymin=Wales-UK_SE_df$Wales,
-                  ymax=Wales+UK_SE_df$Wales),alpha=0.4)+
+  geom_ribbon(aes(ymin=Wales-Park_UK_SE$Wales,
+                  ymax=Wales+Park_UK_SE$Wales),alpha=0.4)+
   coord_cartesian(ylim=c(-60,160))+
   geom_hline(yintercept=0)+ 
   theme(panel.background = element_rect(fill = "white", colour = "black",
@@ -110,10 +92,10 @@ WAL_graph<-
 
 #Scotland
 SCOT_graph<-
-  ggplot(data=UK_mean_df,aes(x=Date,y=Scotland))+
+  ggplot(data=Park_UK_mean,aes(x=Date,y=Scotland))+
   geom_line()+
-  geom_ribbon(aes(ymin=Scotland-UK_SE_df$Scotland,
-                  ymax=Scotland+UK_SE_df$Scotland),alpha=0.4)+
+  geom_ribbon(aes(ymin=Scotland-Park_UK_SE$Scotland,
+                  ymax=Scotland+Park_UK_SE$Scotland),alpha=0.4)+
   coord_cartesian(ylim=c(-60,160))+
   geom_hline(yintercept=0)+ 
   theme(panel.background = element_rect(fill = "white", colour = "black",
@@ -126,10 +108,10 @@ SCOT_graph<-
 
 #Northern Ireland
 NI_graph<-
-  ggplot(data=UK_mean_df,aes(x=Date,y=N_Ireland))+
+  ggplot(data=Park_UK_mean,aes(x=Date,y=N_Ireland))+
   geom_line()+
-  geom_ribbon(aes(ymin=N_Ireland-UK_SE_df$N_Ireland,
-                  ymax=N_Ireland+UK_SE_df$N_Ireland),alpha=0.4)+
+  geom_ribbon(aes(ymin=N_Ireland-Park_UK_SE$N_Ireland,
+                  ymax=N_Ireland+Park_UK_SE$N_Ireland),alpha=0.4)+
   coord_cartesian(ylim=c(-60,160))+
   geom_hline(yintercept=0)+ 
   theme(panel.background = element_rect(fill = "white", colour = "black",
