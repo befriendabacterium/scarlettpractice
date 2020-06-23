b8237b86b41e4dbf04f51bb8a0d383578c6d5525
Matt Lloyd Jones <befriendabacterium@users.noreply.github.com>
2020-06-23 10:41:04 +0100

    changed plots to plot as bar graphs

--- code/GoogleMobility_percountry.R
+++ code/GoogleMobility_percountry.R
@@ -66,9 +66,6 @@ Park_UK_SE$Date<-as.Date(Park_UK_SE$Date)
 # Plots for the whole of time series avalible to us -------------------------------------------------------------------
 #Creates the graph referring to England districts. 
 
-
-barplot(Park_UK_mean$England~seq(0.5,120.5,1))
-
 ENG_graph<-
   ggplot(data=Park_UK_mean,aes(x=Date,y=England, group=Date)) +
 
@@ -91,55 +88,77 @@ ENG_graph<-
     
   ggtitle("England")+xlab("Date")+ylab("Parks percentage change from baseline")
 
-ENG_graph
 
 #Creates the graph for the Wales districts
 WAL_graph<-
-  ggplot(data=Park_UK_mean,aes(x=Date,y=Wales))+
-  geom_line()+
-  geom_ribbon(aes(ymin=Wales-Park_UK_SE$Wales,
-                  ymax=Wales+Park_UK_SE$Wales),alpha=0.4)+
-  coord_cartesian(ylim=c(-60,160))+
-  geom_hline(yintercept=0)+ 
+  ggplot(data=Park_UK_mean,aes(x=Date,y=Wales, group=Date)) +
+  
+  geom_col(position = position_dodge(width=0.2)) +
+  
+  geom_errorbar(aes(ymin=Wales-Park_UK_SE$Wales,
+                    ymax=Wales+Park_UK_SE$Wales), 
+                width=0, position = position_dodge(width=0.9)) +
+  
+  coord_cartesian(ylim=c(-60,160)) +
+  
+  geom_hline(yintercept=0) + 
+  
   theme(panel.background = element_rect(fill = "white", colour = "black",
                                         size = 1, linetype = "solid"),
         panel.grid.major = element_line(size = 0.5, linetype = 'solid',
                                         colour = "grey"), 
         panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "grey"))+
+                                        colour = "grey")) +
+  
   ggtitle("Wales")+xlab("Date")+ylab("Parks percentage change from baseline")
 
-#Creates a graph for Scotlands' districts.
+#Creates a graph for Scotland' district
 SCOT_graph<-
-  ggplot(data=Park_UK_mean,aes(x=Date,y=Scotland))+
-  geom_line()+
-  geom_ribbon(aes(ymin=Scotland-Park_UK_SE$Scotland,
-                  ymax=Scotland+Park_UK_SE$Scotland),alpha=0.4)+
-  coord_cartesian(ylim=c(-60,160))+
-  geom_hline(yintercept=0)+ 
+  ggplot(data=Park_UK_mean,aes(x=Date,y=Scotland, group=Date)) +
+  
+  geom_col(position = position_dodge(width=0.2)) +
+  
+  geom_errorbar(aes(ymin=Scotland-Park_UK_SE$Scotland,
+                    ymax=Scotland+Park_UK_SE$Scotland), 
+                width=0, position = position_dodge(width=0.9)) +
+  
+  coord_cartesian(ylim=c(-60,160)) +
+  
+  geom_hline(yintercept=0) + 
+  
   theme(panel.background = element_rect(fill = "white", colour = "black",
                                         size = 1, linetype = "solid"),
         panel.grid.major = element_line(size = 0.5, linetype = 'solid',
                                         colour = "grey"), 
         panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "grey"))+
+                                        colour = "grey")) +
+  
   ggtitle("Scotland")+xlab("Date")+ylab("Parks percentage change from baseline")
+
 
 #Creates a graph for Northern Irelands' district
 NI_graph<-
-  ggplot(data=Park_UK_mean,aes(x=Date,y=N_Ireland))+
-  geom_line()+
-  geom_ribbon(aes(ymin=N_Ireland-Park_UK_SE$N_Ireland,
-                  ymax=N_Ireland+Park_UK_SE$N_Ireland),alpha=0.4)+
-  coord_cartesian(ylim=c(-60,160))+
-  geom_hline(yintercept=0)+ 
+  ggplot(data=Park_UK_mean,aes(x=Date,y=Scotland, group=Date)) +
+  
+  geom_col(position = position_dodge(width=0.2)) +
+  
+  geom_errorbar(aes(ymin=Scotland-Park_UK_SE$Scotland,
+                    ymax=Scotland+Park_UK_SE$Scotland), 
+                width=0, position = position_dodge(width=0.9)) +
+  
+  coord_cartesian(ylim=c(-60,160)) +
+  
+  geom_hline(yintercept=0) + 
+  
   theme(panel.background = element_rect(fill = "white", colour = "black",
                                         size = 1, linetype = "solid"),
         panel.grid.major = element_line(size = 0.5, linetype = 'solid',
                                         colour = "grey"), 
         panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "grey"))+
-  ggtitle("N Ireland")+xlab("Date")+ylab("Parks percentage change from baseline")
+                                        colour = "grey")) +
+  
+  ggtitle("NI")+xlab("Date")+ylab("Parks percentage change from baseline")
+
 
 #This plots all the graphs onto one image.
 grid.arrange(ENG_graph,WAL_graph,SCOT_graph,NI_graph, nrow = 2)
@@ -151,70 +170,101 @@ Park_UK_SE_may<-Park_UK_SE[Park_UK_SE$Date >= "2020-05-01" & Park_UK_SE$Date <= 
 
 # May Plots -------------------------------------------------------------------
 #Creates a graph for Englands' districts.
+
 ENG_graph_may<-
-  ggplot(data=Park_UK_mean_may,aes(x=Date,y=England))+
-  geom_line()+
-  geom_ribbon(aes(ymin=England-Park_UK_SE_may$England,
-                  ymax=England+Park_UK_SE_may$England),alpha=0.4)+
-  coord_cartesian(ylim=c(-60,160))+
-  geom_hline(yintercept=0)+ 
+  ggplot(data=Park_UK_mean_may,aes(x=Date,y=England, group=Date)) +
+  
+  geom_col(position = position_dodge(width=0.2)) +
+  
+  geom_errorbar(aes(ymin=England-Park_UK_SE_may$England,
+                    ymax=England+Park_UK_SE_may$England), 
+                width=0, position = position_dodge(width=0.9)) +
+  
+  coord_cartesian(ylim=c(-60,160)) +
+  
+  geom_hline(yintercept=0) + 
+  
   theme(panel.background = element_rect(fill = "white", colour = "black",
                                         size = 1, linetype = "solid"),
         panel.grid.major = element_line(size = 0.5, linetype = 'solid',
                                         colour = "grey"), 
         panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "grey"))+
+                                        colour = "grey")) +
+  
   ggtitle("England")+xlab("Date")+ylab("Parks percentage change from baseline")
 
 
-#Creates a graph for Wales' districts
+#Creates the graph for the Wales districts
 WAL_graph_may<-
-  ggplot(data=Park_UK_mean_may,aes(x=Date,y=Wales))+
-  geom_line()+
-  geom_ribbon(aes(ymin=Wales-Park_UK_SE_may$Wales,
-                  ymax=Wales+Park_UK_SE_may$Wales),alpha=0.4)+
-  coord_cartesian(ylim=c(-60,160))+
-  geom_hline(yintercept=0)+ 
+  ggplot(data=Park_UK_mean_may,aes(x=Date,y=Wales, group=Date)) +
+  
+  geom_col(position = position_dodge(width=0.2)) +
+  
+  geom_errorbar(aes(ymin=Wales-Park_UK_SE_may$Wales,
+                    ymax=Wales+Park_UK_SE_may$Wales), 
+                width=0, position = position_dodge(width=0.9)) +
+  
+  coord_cartesian(ylim=c(-60,160)) +
+  
+  geom_hline(yintercept=0) + 
+  
   theme(panel.background = element_rect(fill = "white", colour = "black",
                                         size = 1, linetype = "solid"),
         panel.grid.major = element_line(size = 0.5, linetype = 'solid',
                                         colour = "grey"), 
         panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "grey"))+
+                                        colour = "grey")) +
+  
   ggtitle("Wales")+xlab("Date")+ylab("Parks percentage change from baseline")
 
-#Creates a graph for Scotlands' districts
+#Creates a graph for Scotland' district
 SCOT_graph_may<-
-  ggplot(data=Park_UK_mean_may,aes(x=Date,y=Scotland))+
-  geom_line()+
-  geom_ribbon(aes(ymin=Scotland-Park_UK_SE_may$Scotland,
-                  ymax=Scotland+Park_UK_SE_may$Scotland),alpha=0.4)+
-  coord_cartesian(ylim=c(-60,160))+
-  geom_hline(yintercept=0)+ 
+  ggplot(data=Park_UK_mean_may,aes(x=Date,y=Scotland, group=Date)) +
+  
+  geom_col(position = position_dodge(width=0.2)) +
+  
+  geom_errorbar(aes(ymin=Scotland-Park_UK_SE_may$Scotland,
+                    ymax=Scotland+Park_UK_SE_may$Scotland), 
+                width=0, position = position_dodge(width=0.9)) +
+  
+  coord_cartesian(ylim=c(-60,160)) +
+  
+  geom_hline(yintercept=0) + 
+  
   theme(panel.background = element_rect(fill = "white", colour = "black",
                                         size = 1, linetype = "solid"),
         panel.grid.major = element_line(size = 0.5, linetype = 'solid',
                                         colour = "grey"), 
         panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "grey"))+
+                                        colour = "grey")) +
+  
   ggtitle("Scotland")+xlab("Date")+ylab("Parks percentage change from baseline")
 
-#Creates a graph for Northern Irelands' districts
+
+#Creates a graph for Northern Irelands' district
 NI_graph_may<-
-  ggplot(data=Park_UK_mean_may,aes(x=Date,y=N_Ireland))+
-  geom_line()+
-  geom_ribbon(aes(ymin=N_Ireland-Park_UK_SE_may$N_Ireland,
-                  ymax=N_Ireland+Park_UK_SE_may$N_Ireland),alpha=0.4)+
-  coord_cartesian(ylim=c(-60,160))+
-  geom_hline(yintercept=0)+ 
+  ggplot(data=Park_UK_mean_may,aes(x=Date,y=Scotland, group=Date)) +
+  
+  geom_col(position = position_dodge(width=0.2)) +
+  
+  geom_errorbar(aes(ymin=Scotland-Park_UK_SE_may$Scotland,
+                    ymax=Scotland+Park_UK_SE_may$Scotland), 
+                width=0, position = position_dodge(width=0.9)) +
+  
+  coord_cartesian(ylim=c(-60,160)) +
+  
+  geom_hline(yintercept=0) + 
+  
   theme(panel.background = element_rect(fill = "white", colour = "black",
                                         size = 1, linetype = "solid"),
         panel.grid.major = element_line(size = 0.5, linetype = 'solid',
                                         colour = "grey"), 
         panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "grey"))+
-  ggtitle("N Ireland")+xlab("Date")+ylab("Parks percentage change from baseline")
+                                        colour = "grey")) +
+  
+  ggtitle("NI")+xlab("Date")+ylab("Parks percentage change from baseline")
 
-#Creates one plot that contains all the four countrys graphs. 
+
+#This plots all the graphs onto one image.
 grid.arrange(ENG_graph_may,WAL_graph_may,SCOT_graph_may,NI_graph_may, nrow = 2)
 
